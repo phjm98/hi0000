5dea734ed84beee279d0610a306b9abb3f5dd258
phjm98 <wholesalea565@gmail.com>
2025-05-26 21:15:27 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".bars-menu").addEventListener("click", () => {
-  document.querySelector(".nav-items").classList.toggle("active");
-});
-
-document.querySelectorAll(".nav-link").forEach((item) => {
-  item.addEventListener("click", () => {
-    document.querySelector(".nav-items").classList.toggle("active");
-  });
-});
